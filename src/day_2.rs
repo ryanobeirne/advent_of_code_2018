6124990098557226743cfa21d7a0ccbbcdd4dd67
Ryan O'Beirne <ryanobeirne@gmail.com>
2018-12-10 17:35:51 -0500

    Complete Day 2, Part 2

--- src/day_2.rs
+++ src/day_2.rs
@@ -1,6 +1,10 @@
 pub fn go() {
     part_1();
-    // part_2();
+    part_2();
+}
+
+fn characterize(s: &String) -> Vec<char> {
+    s.chars().collect()
 }
 
 fn part_1() {
@@ -13,7 +17,7 @@ fn part_1() {
 
     // Does the string contain exactly n characters
     fn count_chars(s: &String, n: isize) -> bool {
-        let vec_char: Vec<char> = s.chars().collect();
+        let vec_char = characterize(s);
         if vec_char.len() == 0 || n == 0 { return false }
 
         for character in &vec_char {
@@ -48,4 +52,43 @@ fn part_1() {
     let checksum = count_2 * count_3;
 
     println!("\tPart 1: {}", checksum);
+}
+
+fn part_2() {
+    use std::fmt::Write;
+
+    let lines = super::input::read(2);
+    let mut diff_vec: Vec<&String> = Vec::new();
+
+    for id in lines.iter() {
+        let chars = characterize(&id);
+        for id2 in lines.iter() {
+            let chars2 = characterize(&id2);
+            if chars == chars2 { continue }
+            let mut diff_count = 0;
+
+            for (index, c) in chars.iter().enumerate() {
+                if c != &chars2[index] {
+                    diff_count += 1;
+                }
+            }
+
+            if diff_count == 1 {
+                diff_vec.push(id);
+            }
+        }
+    }
+
+    let mut s = String::new();
+
+    let char_vec1 = characterize(diff_vec[0]);
+    let char_vec2 = characterize(diff_vec[1]);
+
+    for (i, c) in char_vec1.iter().enumerate() {
+        if c == &char_vec2[i] {
+            write!(&mut s, "{}", c);
+        }
+    }
+
+    println!("\tPart 2: {}", s);
 }
