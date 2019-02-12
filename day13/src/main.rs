c6faf444d46e778d6a0b64f48d3eadffac24091a
Ryan O'Beirne <robeirne@spoonflower.com>
2019-02-12 10:37:06 -0500

    Day 13, Part 2

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -27,14 +27,43 @@ fn main() -> Result<()> {
     let part_1 = part1(&mut map.clone());
     println!("Part 1: {},{}", part_1.x, part_1.y);
 
+    let part_2 = part2(&mut map.clone());
+    println!("Part 2: {},{}", part_2.x, part_2.y);
+
     Ok(())
+}
+
+#[cfg(debug_assertions)]
+fn sleep(sec: u64) {
+    std::thread::sleep(std::time::Duration::from_secs(sec))
 }
 
 fn part1(map: &mut Map) -> Location {
     loop {
+        #[cfg(debug_assertions)] {
+            println!("{}", map);
+            sleep(1);
+        }
+
         if let Some(loc) = map.tick() {
             break loc;
         }
+    }
+}
+
+fn part2(map: &mut Map) -> Location {
+    loop {
+        #[cfg(debug_assertions)] {
+            println!("{}", map);
+            sleep(1);
+        }
+
+        if map.cart_count() <= 1 {
+            break map.first_cart_loc();
+        }
+
+        map.tick();
+
     }
 }
 
@@ -46,46 +75,89 @@ impl Map {
         Map(BTreeMap::new())
     }
 
-    fn tick(&mut self) -> Option<Location> {
-        let mut carts = Vec::<(Location, Option<Cart>, Feature)>::new();
-
-        for (loc, pos) in self.0.iter_mut() {
-            if let Some(_cart) = pos.cart {
-                let (new_loc, new_cart) = loc.move_cart(pos);
-                carts.push((new_loc, new_cart, pos.feature));
-                pos.cart = None;
-            }
-        }
-
-        let collision = Map::collision_check(&carts);
-        if collision.is_some() {
-            return collision
-        }
-
-        for (loc, cart, _feature) in carts.iter() {
-            let mut position = self.0.get(&loc).expect("Location is not in map!").clone();
-            position.cart = *cart;
-
-            self.0.insert(*loc, position);
-        }
-        
-        None
+    fn get(&self, loc: &Location) -> &Position {
+        self.0.get(loc).expect("Location is not in map!")
     }
 
-    fn collision_check(carts: &Vec<(Location, Option<Cart>, Feature)>) -> Option<Location> {
-        let mut counter = BTreeMap::<Location, usize>::new();
+    fn tick(&mut self) -> Option<Location> {
+        let cart_locs = self.cart_locs();
+        let collisions = self.collision_check(cart_locs);
 
-        for (loc, _cart, _feature) in carts {
-            *counter.entry(*loc).or_insert(0) += 1;
+        if collisions.is_empty() {
+            None
+        } else {
+            Some(collisions[0])
         }
+    }
 
-        for (loc, count) in counter.iter() {
-            if *count > 1 {
-                return Some(*loc);
+    fn cart_locs(&self) -> Vec<Location> {
+        self.0.iter()
+            .filter(|(_loc, pos)| pos.cart.is_some())
+            .map(|(loc, _pos)| *loc)
+            .collect()
+    }
+
+    fn collision_check(&mut self, cart_locs: Vec<Location>) -> Vec<Location> {
+        let mut collisions = Vec::<Location>::new();
+
+        for location in cart_locs.iter() {
+            let mut cart = self.remove_cart(location);
+            // println!("Before Mutation: {:?}", &cart);
+
+            if let Some((new_loc, new_cart)) = self.get_new_loc(location, &mut cart) {
+                // println!("       Mutation: {:?}", Some(&new_cart));
+                if self.has_cart(&new_loc) {
+                    collisions.push(new_loc);
+                    self.remove_cart(&new_loc);
+                } else {
+                    self.insert_cart(&new_loc, new_cart);
+                }
             }
         }
 
-        None
+        collisions
+    }
+
+    fn get_new_loc(&self, loc: &Location, cart: &mut Option<Cart>) -> Option<(Location, Cart)> {
+        if let Some(mut c) = cart {
+            let feature = &self.get(loc).feature;
+            Some(loc.move_cart(&mut c, feature))
+        } else {
+            None
+        }
+    }
+
+    fn remove_cart(&mut self, loc: &Location) -> Option<Cart> {
+        if let Some(pos) = self.0.remove(loc) {
+            self.0.insert(*loc, Position { feature: pos.feature, cart: None });
+            pos.cart
+        } else {
+            None
+        }
+    }
+
+    fn insert_cart(&mut self, loc: &Location, cart: Cart) {
+        if let Some(mut position) = self.0.remove(loc) {
+            position.cart = Some(cart);
+            self.0.insert(*loc, position);
+        }
+    }
+
+    fn cart_count(&self) -> usize {
+        self.0.values()
+            .filter(|pos| pos.cart.is_some())
+            .count()
+    }
+
+    fn first_cart_loc(&self) -> Location {
+        self.0.iter()
+            .filter(|(_loc, pos)| pos.cart.is_some())
+            .map(|(loc, _pos)| *loc)
+            .nth(0).expect("Map has no carts!")
+    }
+
+    fn has_cart(&self, loc: &Location) -> bool {
+        self.get(loc).cart.is_some()
     }
 
     fn width(&self) -> u8 {
@@ -126,13 +198,13 @@ impl Map {
     }
 }
 
-#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 struct Cart {
     heading: Heading,
     next_turn: Turn,
 }
 
-#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Copy)]
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Copy)]
 enum Heading {
     North,
     South,
@@ -140,7 +212,7 @@ enum Heading {
     West,
 }
 
-#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 enum Turn {
     Left,
     Straight,
@@ -153,7 +225,7 @@ struct Location {
     x: u8,
 }
 
-#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 struct Position {
     feature: Feature,
     cart: Option<Cart>,
@@ -168,7 +240,7 @@ impl Default for Position {
     }
 }
 
-#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 enum Feature {
     Track(Orientation),
     Intersection, // +
@@ -176,34 +248,28 @@ enum Feature {
     Empty,
 }
 
-#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 enum Orientation {
     Horizontal, // -
     Vertical,   // |
 }
 
-#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 enum Rotation {
     Clockwise,        // `/`
     CounterClockwise, // `\`
 }
 
 impl Location {
-    fn move_cart(self, pos: &mut Position) -> (Location, Option<Cart>) {
-        if let Some(mut cart) = pos.cart {
-
-            match pos.feature {
+    fn move_cart(self, cart: &mut Cart, feat: &Feature)  -> (Location, Cart) {
+            match feat {
                 Track(_) => (),
                 Intersection => cart.turn(),
                 Curve(dir) => cart.curve(dir),
                 Empty => panic!("Cart has come off the tracks!"),
             }
 
-            (self.move_by_heading(cart.heading), Some(cart))
-        } else {
-            (self, pos.cart)
-        }
-        
+            (self.move_by_heading(cart.heading), *cart)
     }
 
     fn move_by_heading(&self, heading: Heading) -> Location {
@@ -269,7 +335,7 @@ impl Cart {
         self.next_turn = self.next_turn.next();
     }
 
-    fn curve(&mut self, dir: Rotation) {
+    fn curve(&mut self, dir: &Rotation) {
         self.heading = self.heading.curve(dir);
     }
 }
@@ -277,7 +343,7 @@ impl Cart {
 impl Heading {
     fn turn(&self, turn: &Turn) -> Heading {
         match (self, turn) {
-            (_, Straight) => self.clone(),
+            (_, Straight) => *self,
             (North, Right) | (South, Left) => East,
             (East,  Right) | (West,  Left) => South,
             (South, Right) | (North, Left) => West,
@@ -285,7 +351,7 @@ impl Heading {
         }
     }
 
-    fn curve(&self, rotation: Rotation) -> Heading {
+    fn curve(&self, rotation: &Rotation) -> Heading {
         match (self, rotation) {
             (North, Clockwise) | (South, CounterClockwise) => East,
             (North, CounterClockwise) | (South, Clockwise) => West,
@@ -322,10 +388,11 @@ impl FromStr for Feature {
 
 impl fmt::Display for Map {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let width = self.width();
         write!(f, "{}",
             self.0.iter()
                 .map(|(loc, pos)| {
-                    if loc.x >= self.width() {
+                    if loc.x >= width {
                         format!("{}\n", pos)
                     } else {
                         pos.to_string()
